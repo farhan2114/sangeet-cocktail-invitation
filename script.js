74edb64525f99a54e217be59326d5ce6e23f82b0
farhan2114 <farhan140805@gmail.com>
2026-09-22 22:40:20 +0530

    Optimize Google Sheets RSVP sync with preflight-free Content-Type

--- script.js
+++ script.js
@@ -416,7 +416,7 @@ async function handleRsvpSubmit(event) {
         method: 'POST',
         mode: 'no-cors',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'text/plain;charset=utf-8'
         },
         body: JSON.stringify(rsvpData)
       });
